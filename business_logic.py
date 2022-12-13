62f7392634c7a305c105e4cb7ee77736203955e3
Ivan Pavlyukevich <serpavminsk59@gmail.com>
2022-12-13 13:30:45 +0300

    added changing users

--- business_logic.py
+++ business_logic.py
@@ -2,76 +2,134 @@ from PIL import Image, ImageOps
 import os
 
 
-def MakeIcon(file):
-    PATH = os.path.dirname(os.path.realpath(__file__))
-    mask = Image.open(os.path.join(PATH, "images", "mask.png")).convert('L')
-    im = Image.open(os.path.join(PATH, "images", file))
-    output = ImageOps.fit(im, mask.size, centering=(0.5, 0.5))
-    output.putalpha(mask)
-    return output
+class UserInfo:
+    speciality = "Специальность"
+    faculty = "Факультет"
+    full_name = "ФИО"
+    email = "E-mail"
+    home_phone = "Home Phone"
+    mobile_phone = "Mobile Phone"
+    address = "Address"
+    number_course = "Course number"
+    social_media = "Social Media"
+    bio = "Bio"
+    other1 = "Other info"
+    other2 = "Other info"
+    rating = "Rating"
+    skills = {'Front-end': 0, 'Soft Skills': 0, 'Hard Skills': 0}
+    profile_pic = 'profile_pic.jpg'
 
+    @staticmethod
+    def MakeIcon(file):
+        PATH = os.path.dirname(os.path.realpath(__file__))
+        mask = Image.open(os.path.join(PATH, "images", "mask.png")).convert('L')
+        im = Image.open(os.path.join(PATH, "images", file))
+        output = ImageOps.fit(im, mask.size, centering=(0.5, 0.5))
+        output.putalpha(mask)
+        return output
 
-def get_speciality():
-    return 'Искусственный интеллект'
+    def get_speciality(self):
+        return self.speciality
 
+    def get_faculty(self):
+        return self.faculty
 
-def get_faculty():
-    return 'ФИТиУ'
+    def edit(self):
+        pass
 
+    def get_full_name(self):
+        return self.full_name
 
-def edit():
-    return None
+    def get_email(self):
+        return self.email
 
+    def get_Home_Phone(self):
+        return self.home_phone
 
-def get_full_name():
-    return 'Якубович Никита'
+    def get_Mobile(self):
+        return self.mobile_phone
 
+    def get_Address(self):
+        return self.address
 
-def get_email():
-    return '0101010100@gmail.com'
+    def get_number_course(self):
+        return self.number_course
 
+    def get_social_networks(self):
+        return self.social_media
 
-def get_Home_Phone():
-    return '80(17)-01-01-012'
+    def get_other_info_1(self):
+        return self.other1
 
+    def get_other_info_2(self):
+        return self.other2
 
-def get_Mobile():
-    return '+375(29)111-11-11'
+    def get_rating(self):
+        return self.rating
 
+    def get_skills(self):
+        return self.skills
 
-def get_Adress():
-    return 'Dzerzhinskogo 95, Minsk'
+    def get_note(self):
+        return self.bio
 
-
-def get_number_course():
-    return 'Курс: 3'
-
-
-def get_social_networks():
-    return 'Instagram: @_nkch_n'
-
-
-def get_other_info_1():
-    return 'Other info'
-
-
-def get_other_info_2():
-    return 'Other info'
-
-
-def get_rating():
-    return 'Рейтинг: 4'
-
-
-def get_skills():
-    return {'Front-end': 4, 'Soft Skills': 3, 'Hard Skills': 4}
-
-
-def get_note():
-    return ''' Пусть,
-оскалясь короной,
-вздымает британский лев вой.
-Коммуне не быть покорённой.
-Левой!
-Левой!
-Левой!'''
+    def change_user(self, user_id):
+        if user_id == 0:
+            self.speciality = "Искусственный интеллект"
+            self.faculty = "ФИТиУ"
+            self.full_name = "Якубович Никита"
+            self.email = "nikita_yakub@gmail.com"
+            self.home_phone = "999 99 99"
+            self.mobile_phone = "+375 29 111 11 11"
+            self.address = "г. Минск, пр. Дзержинского, д. 35"
+            self.number_course = "Курс: 3"
+            self.social_media = "Instagram: @_nkch_n"
+            self.bio = '''
+            Пусть,
+            оскалясь короной,
+            вздымает британский лев вой.
+            Коммуне не быть покорённой.
+            Левой!
+            Левой!
+            Левой!'''
+            self.other1 = ""
+            self.other2 = ""
+            self.rating = "Рейтинг: 4"
+            self.skills = {'Front-end': 4, 'Soft Skills': 3, 'Hard Skills': 4}
+            self.profile_pic = 'profile_beans.png'
+        elif user_id == 1:
+            self.speciality = "Кафедра ИИТ"
+            self.faculty = "ФИТиУ"
+            self.full_name = "Петров Пётр Петрович"
+            self.email = "petr_petr@gmail.com"
+            self.home_phone = "312 83 22"
+            self.mobile_phone = "+375 29 812 92 22"
+            self.address = "г. Минск, ул. Платонова, д. 1"
+            self.number_course = "Образование: Политех, 1999"
+            self.social_media = "twitter.com/petr_petr"
+            self.bio = """
+            Падение - это не провал.
+            Провал - это провал.
+            Падение - это где упал.
+            """
+            self.other1 = ""
+            self.other2 = ""
+            self.rating = "0% пересдач"
+            self.skills = {'Знания': 1, 'Преподавание': 3, 'Личность': 4}
+            self.profile_pic = 'profile_hamster.jpg'
+        else:
+            self.speciality = "Специальность"
+            self.faculty = "Факультет"
+            self.full_name = "ФИО"
+            self.email = "E-mail"
+            self.home_phone = "Home Phone"
+            self.mobile_phone = "Mobile Phone"
+            self.address = "Address"
+            self.number_course = "Course number"
+            self.social_media = "Social Media"
+            self.bio = "Bio"
+            self.other1 = "Other info"
+            self.other2 = "Other info"
+            self.rating = "Rating"
+            self.skills = {'Front-end': 0, 'Soft Skills': 0, 'Hard Skills': 0}
+            self.profile_pic = 'profile_pic.jpg'
--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import tkinter.messagebox
 import customtkinter as ctk
 import os
 from PIL import Image, ImageTk
-import business_logic
+from business_logic import UserInfo
 from tkinter import ttk
+import requests
 
 PATH = os.path.dirname(os.path.realpath(__file__))
 DEFAULT_FONT = ""
@@ -14,7 +15,7 @@ ctk.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark
 
 
 class StartingScreen(ctk.CTk):
-    WIDTH = 845
+    WIDTH = 911
     HEIGHT = 550
 
     def __init__(self):
@@ -86,8 +87,9 @@ class StartingScreen(ctk.CTk):
 
         self.init_authorization_screen()
         self.init_user_info_UI()
+        self.init_timetable_screen()
         self.hide_all_windows()
-        self.navigate_home()
+        self.navigate_select_user()
 
     def init_user_info_UI(self):
         self.user_info = ctk.CTkFrame(master=self.content_frame, fg_color='#E8E8E8')
@@ -101,21 +103,25 @@ class StartingScreen(ctk.CTk):
                                               fg_color='#F8F9F9')
         self.user_info_profile.grid(row=0, column=1, sticky="nsew", padx=5, pady=(10, 5))
 
-        image = business_logic.MakeIcon('profile_pic.jpg')
-        icon_image = ctk.CTkImage(light_image=image, size=(image.size[0]/1.5, image.size[1]/1.5))
-        self.label_icon = ctk.CTkLabel(master=self.user_info_profile, text='', image=icon_image).pack(pady=15)
-        ctk.CTkLabel(master=self.user_info_profile, text=business_logic.get_speciality(),
-                     text_color='grey', font=("Segoe UI", 13)).pack()
-        ctk.CTkLabel(master=self.user_info_profile, text=business_logic.get_faculty(),
-                     text_color='grey', font=("Segoe UI", 13)).pack()
+        image = userinfo.MakeIcon(userinfo.profile_pic)
+        icon_image = ctk.CTkImage(light_image=image, size=(image.size[0] / 1.5, image.size[1] / 1.5))
+        self.label_icon = ctk.CTkLabel(master=self.user_info_profile, text='', image=icon_image)
+        self.label_icon.pack(pady=15)
+        self.speciality = ctk.CTkLabel(master=self.user_info_profile, text=userinfo.get_speciality(),
+                                       text_color='grey', font=("Segoe UI", 13))
+        self.speciality.pack()
+        self.faculty = ctk.CTkLabel(master=self.user_info_profile, text=userinfo.get_faculty(),
+                                    text_color='grey', font=("Segoe UI", 13))
+        self.faculty.pack()
         self.button_edit = ctk.CTkButton(master=self.user_info_profile, text='Изменить', font=("Segoe UI", 13),
                                          fg_color='#0090DE', text_color='white',
-                                         command=business_logic.edit).pack(pady=15)
+                                         command=userinfo.edit).pack(pady=15)
 
         # =========================== user contacts            ===========================
         self.user_info_basic = ctk.CTkFrame(master=self.user_info, border_color="#c5c7c4", border_width=0.5,
                                             fg_color='#F8F9F9')
         self.user_info_basic.grid(row=0, column=2, columnspan=2, sticky="nsew", padx=5, pady=(10, 5))
+        self.user_info_basic.columnconfigure((0, 1, 2), weight=1)
 
         ctk.CTkLabel(master=self.user_info_basic, text='Full Name', font=("Segoe UI", 13)).grid(column=0, row=0,
                                                                                                 padx=30, pady=10,
@@ -128,46 +134,50 @@ class StartingScreen(ctk.CTk):
         ctk.CTkLabel(master=self.user_info_basic, text='Mobile Phone', font=("Segoe UI", 13)).grid(column=0, row=6,
                                                                                                    padx=30, pady=10,
                                                                                                    sticky='w')
-        ctk.CTkLabel(master=self.user_info_basic, text='Adress', font=("Segoe UI", 13)).grid(column=0, row=8, padx=30,
-                                                                                             pady=10, sticky='w')
+        ctk.CTkLabel(master=self.user_info_basic, text='Address', font=("Segoe UI", 13)).grid(column=0, row=8, padx=30,
+                                                                                              pady=10, sticky='w')
 
         for i in [1, 3, 5, 7, 9]:
             ttk.Separator(master=self.user_info_basic, orient='horizontal').grid(column=0, columnspan=3, row=i,
                                                                                  sticky='ewns')
 
-        ctk.CTkLabel(master=self.user_info_basic, text=business_logic.get_full_name(), font=("Segoe UI", 13),
-                     text_color='grey').grid(column=2, row=0, sticky='w', padx=165, pady=10)
-        ctk.CTkLabel(master=self.user_info_basic, text=business_logic.get_email(), font=("Segoe UI", 13),
-                     text_color='grey').grid(column=2, row=2, sticky='w', padx=165, pady=10)
-        ctk.CTkLabel(master=self.user_info_basic, text=business_logic.get_Home_Phone(), font=("Segoe UI", 13),
-                     text_color='grey').grid(column=2, row=4, sticky='w', padx=165, pady=10)
-        ctk.CTkLabel(master=self.user_info_basic, text=business_logic.get_Mobile(), font=("Segoe UI", 13),
-                     text_color='grey').grid(column=2, row=6, sticky='w', padx=165, pady=10)
-        ctk.CTkLabel(master=self.user_info_basic, text=business_logic.get_Adress(), font=("Segoe UI", 13),
-                     text_color='grey').grid(column=2, row=8, sticky='w', padx=165, pady=10)
+        self.full_name = ctk.CTkLabel(master=self.user_info_basic, text=userinfo.get_full_name(), font=("Segoe UI", 13),
+                                      text_color='grey')
+        self.full_name.grid(column=2, row=0, sticky='w', padx=165, pady=10)
+        self.lbl_email = ctk.CTkLabel(master=self.user_info_basic, text=userinfo.get_email(), font=("Segoe UI", 13),
+                                      text_color='grey')
+        self.lbl_email.grid(column=2, row=2, sticky='w', padx=165, pady=10)
+        self.h_phone = ctk.CTkLabel(master=self.user_info_basic, text=userinfo.get_Home_Phone(), font=("Segoe UI", 13),
+                                    text_color='grey')
+        self.h_phone.grid(column=2, row=4, sticky='w', padx=165, pady=10)
+        self.m_phone = ctk.CTkLabel(master=self.user_info_basic, text=userinfo.get_Mobile(), font=("Segoe UI", 13),
+                                    text_color='grey')
+        self.m_phone.grid(column=2, row=6, sticky='w', padx=165, pady=10)
+        self.address = ctk.CTkLabel(master=self.user_info_basic, text=userinfo.get_Address(), font=("Segoe UI", 13),
+                                    text_color='grey')
+        self.address.grid(column=2, row=8, sticky='w', padx=165, pady=10)
 
         # =========================== user other info           ===========================
         self.user_info_uni = ctk.CTkFrame(master=self.user_info, border_color="#c5c7c4", border_width=0.5,
                                           fg_color='#F8F9F9')
         self.user_info_uni.grid(row=1, column=1, sticky="nsew", padx=5, pady=(5, 10))
+        self.user_info_uni.columnconfigure((0, 1, 2, 3), weight=1)
 
         for i in [1, 3, 5, 7, 9]:
-            ttk.Separator(master=self.user_info_uni, orient='horizontal').grid(column=0, columnspan=3, row=i,
+            ttk.Separator(master=self.user_info_uni, orient='horizontal').grid(column=1, columnspan=2, row=i,
                                                                                sticky='ewns')
 
-        ctk.CTkLabel(master=self.user_info_uni, text=business_logic.get_number_course(), font=("Segoe UI", 13)).grid(
-            column=2, row=0, sticky='ew', padx=40, pady=8)
-        ctk.CTkLabel(master=self.user_info_uni, text=business_logic.get_rating(), font=("Segoe UI", 13)).grid(column=2,
-                                                                                                              row=2,
-                                                                                                              sticky='ew',
-                                                                                                              padx=40,
-                                                                                                              pady=8)
-        ctk.CTkLabel(master=self.user_info_uni, text=business_logic.get_social_networks(), font=("Segoe UI", 13)).grid(
-            column=2, row=4, sticky='ew', padx=40, pady=8)
-        ctk.CTkLabel(master=self.user_info_uni, text=business_logic.get_other_info_1(), font=("Segoe UI", 13)).grid(
-            column=2, row=6, sticky='ew', padx=40, pady=8)
-        ctk.CTkLabel(master=self.user_info_uni, text=business_logic.get_other_info_2(), font=("Segoe UI", 13)).grid(
-            column=2, row=8, sticky='ew', padx=40, pady=8)
+        self.course = ctk.CTkLabel(master=self.user_info_uni, text=userinfo.get_number_course(), font=("Segoe UI", 13))
+        self.course.grid(column=2, row=0, sticky='ew', padx=40, pady=8)
+        self.rating = ctk.CTkLabel(master=self.user_info_uni, text=userinfo.get_rating(), font=("Segoe UI", 13))
+        self.rating.grid(column=2, row=2, sticky='ew', padx=40, pady=8)
+        self.social_networks = ctk.CTkLabel(master=self.user_info_uni, text=userinfo.get_social_networks(),
+                                            font=("Segoe UI", 13))
+        self.social_networks.grid(column=2, row=4, sticky='ew', padx=40, pady=8)
+        self.other1 = ctk.CTkLabel(master=self.user_info_uni, text=userinfo.get_other_info_1(), font=("Segoe UI", 13))
+        self.other1.grid(column=2, row=6, sticky='ew', padx=40, pady=8)
+        self.other2 = ctk.CTkLabel(master=self.user_info_uni, text=userinfo.get_other_info_2(), font=("Segoe UI", 13))
+        self.other2.grid(column=2, row=8, sticky='ew', padx=40, pady=8)
 
         # =========================== user skills          ===========================
         self.user_info_parameters = ctk.CTkFrame(master=self.user_info, border_color="#c5c7c4", border_width=0.5,
@@ -176,20 +186,26 @@ class StartingScreen(ctk.CTk):
 
         ctk.CTkLabel(master=self.user_info_parameters, text='ASSIGNMENT:', font=("Segoe UI", 13),
                      text_color='#0090DE').pack(pady=10, padx=10, anchor='w')
-        list_of_skills = business_logic.get_skills()
+        self.list_of_keys = []
+        self.list_of_progressbars = []
+        list_of_skills = userinfo.get_skills()
         for key, value in list_of_skills.items():
-            ctk.CTkLabel(master=self.user_info_parameters, text=key, font=("Segoe UI", 11)).pack(anchor='w', padx=10)
+            key1 = ctk.CTkLabel(master=self.user_info_parameters, text=key, font=("Segoe UI", 11))
+            key1.pack(anchor='w', padx=10)
+            self.list_of_keys.append(key1)
             pg = ctk.CTkProgressBar(master=self.user_info_parameters, height=12, progress_color='#0090DE',
                                     fg_color='#E8E8E8', corner_radius=4)
             pg.pack(padx=(10, 10), pady=(0, 10))
             pg.set(value / 5)
+            self.list_of_progressbars.append(pg)
 
         # =========================== user notes         ===========================
         self.user_info_bio = ctk.CTkFrame(master=self.user_info, border_color="#c5c7c4", border_width=0.5,
                                           fg_color='#F8F9F9')
         self.user_info_bio.grid(row=1, column=3, sticky="nsew", padx=5, pady=(5, 10))
-        ctk.CTkLabel(master=self.user_info_bio, text=business_logic.get_note(), text_color='grey',
-                     font=("Segoe UI", 13, 'italic')).pack(pady=20, padx=20, anchor='center')
+        self.bio_string = ctk.CTkLabel(master=self.user_info_bio, text=userinfo.get_note(), text_color='grey',
+                                       font=("Segoe UI", 13, 'italic'))
+        self.bio_string.pack(pady=20, padx=20, anchor='center')
 
     def init_authorization_screen(self):
         self.auth_info = ctk.CTkFrame(master=self.content_frame)
@@ -220,9 +236,16 @@ class StartingScreen(ctk.CTk):
                                               font=("Segoe UI", -18), command=self.login)
         self.login_submit_btn.grid(row=4, column=1, sticky="sew", pady=25)
 
+    def init_timetable_screen(self):
+        self.timetable_info = ctk.CTkFrame(master=self.content_frame)
+        self.timetable_info.grid(row=0, column=0)
+        self.btn_notify = ctk.CTkButton(master=self.timetable_info, text="Уведомить об опоздании", command=self.notify)
+        self.btn_notify.grid(row=0, column=0)
+
     def hide_all_windows(self):
         self.user_info.grid_forget()
         self.auth_info.grid_forget()
+        self.timetable_info.grid_forget()
 
     def navigate_home(self):
         self.return_highlighted_texts_to_normal()
@@ -245,6 +268,8 @@ class StartingScreen(ctk.CTk):
         self.return_highlighted_texts_to_normal()
         self.btn_timetable.configure(font=("Segoe UI", -15, "bold"))
         self.hide_all_windows()
+        self.timetable_info.grid(row=0, column=0, sticky="nsew")
+        self.content_frame.grid_propagate(False)
 
     def navigate_select_user(self):
         self.return_highlighted_texts_to_normal()
@@ -260,12 +285,54 @@ class StartingScreen(ctk.CTk):
         self.btn_timetable.configure(font=("Segoe UI", -13))
         self.btn_user.configure(font=("Segoe UI", -13))
 
+    def update_user_info(self):
+        image = userinfo.MakeIcon(userinfo.profile_pic)
+        icon_image = ctk.CTkImage(light_image=image, size=(image.size[0] / 1.5, image.size[1] / 1.5))
+        self.label_icon.configure(image=icon_image)
+        self.bio_string.configure(text=userinfo.get_note())
+        new_skills = userinfo.get_skills()
+        i = 0
+        for key in new_skills.keys():
+            self.list_of_keys[i].configure(text=key)
+            i += 1
+        i = 0
+        for value1 in new_skills.values():
+            self.list_of_progressbars[i].set(value1 / 5)
+            i += 1
+        self.other1.configure(text=userinfo.get_other_info_1())
+        self.other2.configure(text=userinfo.get_other_info_2())
+        self.full_name.configure(text=userinfo.get_full_name())
+        self.lbl_email.configure(text=userinfo.get_email())
+        self.address.configure(text=userinfo.get_Address())
+        self.course.configure(text=userinfo.get_number_course())
+        self.social_networks.configure(text=userinfo.get_social_networks())
+        self.m_phone.configure(text=userinfo.get_Mobile())
+        self.h_phone.configure(text=userinfo.get_Home_Phone())
+        self.rating.configure(text=userinfo.get_rating())
+        self.faculty.configure(text=userinfo.get_faculty())
+        self.speciality.configure(text=userinfo.get_speciality())
+
     def login(self):
         self.email = self.login_email.get()
         self.password = self.login_password.get()
-        print(self.email, self.password)
+        if self.email == "petr_petr@gmail.com":
+            if self.password == "1234":
+                userinfo.change_user(1)
+            else:
+                tk.messagebox.showerror("Ошибка", "Что-то не так!")
+        if self.email == "nikita_yakub@gmail.com":
+            if self.password == "4321":
+                userinfo.change_user(0)
+            else:
+                tk.messagebox.showerror("Ошибка", "Что-то не так!")
+        self.update_user_info()
+
+    def notify(self):
+        pass
 
 
 if __name__ == "__main__":
+    userinfo = UserInfo()
+    userinfo.change_user(-1)
     app = StartingScreen()
     app.mainloop()
